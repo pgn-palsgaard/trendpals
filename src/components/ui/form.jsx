23372b70df51228d6365ff78c0897b4dac027fd3
Peter Grundtvig <pgn@palsgaard.com>
2026-03-03 09:19:37 +0000

    File changes

--- src/components/ui/form.jsx
+++ src/components/ui/form.jsx
@@ -92,7 +92,7 @@ const FormDescription = React.forwardRef(({ className, ...props }, ref) => {
 })
 FormDescription.displayName = "FormDescription"
 
-const FormMessage = React.forwardRef(({ className, children, ...props }, ref) => {
+const FormMessage = React.forwardRef(function FormMessage({ className, children, ...props }, ref) {
   const { error, formMessageId } = useFormField()
   const body = error ? String(error?.message) : children
   if (!body) return null
